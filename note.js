0c5478f45bfdc252a593cfd449a651bdd56b1dc7
Ruhaifi Zainol <47075903+ruhaifie@users.noreply.github.com>
2022-09-12 14:26:57 +0800

    update gitignore

---
+++ note.js
@@ -0,0 +1,15 @@
+/**
+
+*! original 
+rules_version = '2';
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
+      allow read, write: if
+          request.time < timestamp.date(2022, 8, 24);
+    }
+  }
+}
+
+create, read, update, delete
+*/
